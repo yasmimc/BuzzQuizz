7a964a289aaeb766b1dd6dd9f9f47ddf07c24dcc
Yasmim Cavalcanti <yasmimcavalcanti.r@gmail.com>
2021-08-12 20:59:39 -0300

    Added quizz title and questions, both loaded from the API

--- scripts/selected-quizz.js
+++ scripts/selected-quizz.js
@@ -1,7 +1,44 @@
+const quizzScreen = document.querySelector(".screen-2");
+
 function showQuizzScreen(){
 	const quizzesScreen = document.querySelector(".screen-1");
 	quizzesScreen.classList.add("hidden");
-	const quizzScreen = document.querySelector(".screen-2");
+	// const quizzScreen = document.querySelector(".screen-2");
 	quizzScreen.classList.remove("hidden");
 }
 
+showQuizzScreen();
+
+// QUIZZ FOR TEST:
+const friends = axios.get("https://mock-api.bootcamp.respondeai.com.br/api/v3/buzzquizz/quizzes/1");
+friends.then(load);
+
+function load(selectedQuizz){	
+	const quizzContent = quizzScreen.querySelector(".container");
+
+	//Load background image
+	const bgImg = selectedQuizz.data.image;	
+	const quizzHeader = quizzContent.querySelector(".selected-quizz");
+	quizzHeader.style.backgroundImage = `url('${bgImg}')`;
+
+	//Load quizz title
+	const quizzTitle = selectedQuizz.data.title;
+	const title = quizzContent.querySelector("h1");
+	title.innerHTML = `${quizzTitle}`;
+
+	//Load questions
+	const quizzQuestions = selectedQuizz.data.questions;
+	// console.log(quizzQuestions);
+	const quizz = quizzContent.querySelector(".quizz");
+	// console.log(quizz)
+
+	const questions = quizz.querySelectorAll(".question")
+
+	for (let i = 0; i < quizzQuestions.length; i++) {
+		// Load each question 
+		let question = questions[i].querySelector("h1");
+		question.innerHTML = quizzQuestions[i].title;
+	}
+
+
+}
