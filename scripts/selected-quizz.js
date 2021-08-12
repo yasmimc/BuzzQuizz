27fb63a9fb9c2db7080f46fdf13f021c1abe9863
Yasmim Cavalcanti <yasmimcavalcanti.r@gmail.com>
2021-08-12 11:28:43 -0300

    Added function to show screen 2 and hide screen 1

---
+++ scripts/selected-quizz.js
@@ -0,0 +1,7 @@
+function showQuizzScreen(){
+	const quizzesScreen = document.querySelector(".screen-1");
+	quizzesScreen.classList.add("hidden");
+	const quizzScreen = document.querySelector(".screen-2");
+	quizzScreen.classList.remove("hidden");
+}
+
