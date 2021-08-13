4633fc583b4608717840c46bc6468c7c4d01cdd7
giancarvalho <giandcarvalho@gmail.com>
2021-08-13 10:41:17 -0300

    Pasted newer version of screen 1

--- scripts/selected-quizz.js
+++ scripts/selected-quizz.js
@@ -1,60 +1,58 @@
 const quizzScreen = document.querySelector(".screen-2");
 
-function showQuizzScreen(){
-	const quizzesScreen = document.querySelector(".screen-1");
-	quizzesScreen.classList.add("hidden");
-	// const quizzScreen = document.querySelector(".screen-2");
-	quizzScreen.classList.remove("hidden");
+function showQuizzScreen() {
+  const quizzesScreen = document.querySelector(".screen-1");
+  quizzesScreen.classList.add("hidden");
+  // const quizzScreen = document.querySelector(".screen-2");
+  quizzScreen.classList.remove("hidden");
 }
 
 showQuizzScreen();
 
 // QUIZZ FOR TEST:
-const friends = axios.get("https://mock-api.bootcamp.respondeai.com.br/api/v3/buzzquizz/quizzes/1");
+const friends = axios.get(
+  "https://mock-api.bootcamp.respondeai.com.br/api/v3/buzzquizz/quizzes/1"
+);
 friends.then(load);
 
-function load(selectedQuizz){	
-	const quizzContent = quizzScreen.querySelector(".container");
+function load(selectedQuizz) {
+  const quizzContent = quizzScreen.querySelector(".container");
 
-	//Load background image
-	const bgImg = selectedQuizz.data.image;	
-	const quizzHeader = quizzContent.querySelector(".selected-quizz");
-	quizzHeader.style.backgroundImage = `url('${bgImg}')`;
+  //Load background image
+  const bgImg = selectedQuizz.data.image;
+  const quizzHeader = quizzContent.querySelector(".selected-quizz");
+  quizzHeader.style.backgroundImage = `url('${bgImg}')`;
 
-	//Load quizz title
-	const quizzTitle = selectedQuizz.data.title;
-	const title = quizzContent.querySelector("h1");
-	title.innerHTML = `${quizzTitle}`;
+  //Load quizz title
+  const quizzTitle = selectedQuizz.data.title;
+  const title = quizzContent.querySelector("h1");
+  title.innerHTML = `${quizzTitle}`;
 
-	//Load questions
-	const quizzQuestions = selectedQuizz.data.questions;
-	// console.log(quizzQuestions);
-	const quizz = quizzContent.querySelector(".quizz");
-	// console.log(quizz)
+  //Load questions
+  const quizzQuestions = selectedQuizz.data.questions;
+  // console.log(quizzQuestions);
+  const quizz = quizzContent.querySelector(".quizz");
+  // console.log(quizz)
 
-	const questions = quizz.querySelectorAll(".question")
+  const questions = quizz.querySelectorAll(".question");
 
-	for (let i = 0; i < quizzQuestions.length; i++) {
-		// Load each question 
-		let question = questions[i].querySelector("h1");
-		question.innerHTML = quizzQuestions[i].title;
-		question.style.backgroundColor = `${quizzQuestions[i].color}`;
+  for (let i = 0; i < quizzQuestions.length; i++) {
+    // Load each question
+    let question = questions[i].querySelector("h1");
+    question.innerHTML = quizzQuestions[i].title;
+    question.style.backgroundColor = `${quizzQuestions[i].color}`;
 
-		//Load each answer 
-		let options = questions[i].querySelectorAll("button");
-		console.log(options)
-		let answers = quizzQuestions[i].answers;
+    //Load each answer
+    let options = questions[i].querySelectorAll("button");
+    console.log(options);
+    let answers = quizzQuestions[i].answers;
 
-		for (let j = 0; j < answers.length; j++) {
-			console.log(answers[j]);
-			console.log(options[j].innerHTML)
+    for (let j = 0; j < answers.length; j++) {
+      console.log(answers[j]);
+      console.log(options[j].innerHTML);
 
-			options[j].innerHTML = 
-			`<img src="${answers[j].image}">
-			<p>${answers[j].text}</p>`
-		}
-		
-	}
-
-
+      options[j].innerHTML = `<img src="${answers[j].image}">
+			<p>${answers[j].text}</p>`;
+    }
+  }
 }
