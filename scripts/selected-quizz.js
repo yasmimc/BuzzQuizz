fc2a2b869d13f90ad8713ff72ea564a2e2e5104e
Yasmim Cavalcanti <yasmimcavalcanti.r@gmail.com>
2021-08-13 14:10:33 -0300

    modularized some selected-quizz.js functions

--- scripts/selected-quizz.js
+++ scripts/selected-quizz.js
@@ -1,66 +1,70 @@
 const quizzScreen = document.querySelector(".screen-2");
 
 function showQuizzScreen() {
-  const loading = document.querySelector(".loading");
-  const quizzesScreen = document.querySelector(".screen-1");
-  quizzesScreen.classList.add("hidden");
-  //loading.classList.add("hidden");
-  // const quizzScreen = document.querySelector(".screen-2");
-  quizzScreen.classList.remove("hidden");
+	const loading = document.querySelector(".loading");
+	const quizzesScreen = document.querySelector(".screen-1");
+	quizzesScreen.classList.add("hidden");
+	//loading.classList.add("hidden");
+	// const quizzScreen = document.querySelector(".screen-2");
+	quizzScreen.classList.remove("hidden");
 }
 
-showQuizzScreen();
+// showQuizzScreen();
 
 // QUIZZ FOR TEST:
 const friends = axios.get(
-  "https://mock-api.bootcamp.respondeai.com.br/api/v3/buzzquizz/quizzes/1"
+	"https://mock-api.bootcamp.respondeai.com.br/api/v3/buzzquizz/quizzes/1"
 );
 friends.then(load);
 
-function load(selectedQuizz) {
-  //showQuizzScreen();
-  const quizzContent = quizzScreen.querySelector(".container");
+function loadBackgroundImg(quizzContent, selectedQuizz) {
+	const bgImg = selectedQuizz.data.image;
+	const quizzHeader = quizzContent.querySelector(".selected-quizz");
+	quizzHeader.style.backgroundImage = `url('${bgImg}')`;
+}
 
-  //Load background image
-  const bgImg = selectedQuizz.data.image;
-  const quizzHeader = quizzContent.querySelector(".selected-quizz");
-  quizzHeader.style.backgroundImage = `url('${bgImg}')`;
+function loadQuizzTitle(quizzContent, selectedQuizz) {
+	const quizzTitle = selectedQuizz.data.title;
+	const title = quizzContent.querySelector("h1");
+	title.innerHTML = `${quizzTitle}`;
+}
 
-  //Load quizz title
-  const quizzTitle = selectedQuizz.data.title;
-  const title = quizzContent.querySelector("h1");
-  title.innerHTML = `${quizzTitle}`;
+function loadQuizzQuestions(quizzContent, selectedQuizz){
+	//Load questions
+	const quizzQuestions = selectedQuizz.data.questions;
+	const quizz = quizzContent.querySelector(".quizz");
+	const questions = quizz.querySelectorAll(".question");
 
-  //Load questions
-  const quizzQuestions = selectedQuizz.data.questions;
-  // console.log(quizzQuestions);
-  const quizz = quizzContent.querySelector(".quizz");
-  // console.log(quizz)
+	for (let i = 0; i < quizzQuestions.length; i++) {
+		// Load each question
+		let question = questions[i].querySelector("h1");
+		question.innerHTML = quizzQuestions[i].title;
+		question.style.backgroundColor = `${quizzQuestions[i].color}`;
 
-  const questions = quizz.querySelectorAll(".question");
+		//Load each answer
+		let options = questions[i].querySelectorAll("button");
+		console.log(options);
+		let answers = quizzQuestions[i].answers;
 
-  for (let i = 0; i < quizzQuestions.length; i++) {
-    // Load each question
-    let question = questions[i].querySelector("h1");
-    question.innerHTML = quizzQuestions[i].title;
-    question.style.backgroundColor = `${quizzQuestions[i].color}`;
+		//Randomize answers
+		answers.sort(function () {
+			return Math.random() - 0.5;
+		});
 
-    //Load each answer
-    let options = questions[i].querySelectorAll("button");
-    console.log(options);
-    let answers = quizzQuestions[i].answers;
+		for (let j = 0; j < answers.length; j++) {
 
-    //Randomize answers
-    answers.sort(function () {
-      return Math.random() - 0.5;
-    });
-
-    for (let j = 0; j < answers.length; j++) {
-      console.log(answers[j]);
-      console.log(options[j].innerHTML);
-
-      options[j].innerHTML = `<img src="${answers[j].image}">
+			options[j].innerHTML = `<img src="${answers[j].image}">
 			<p>${answers[j].text}</p>`;
-    }
-  }
+		}
+	}
+}
+
+function load(selectedQuizz) {
+	showQuizzScreen();
+
+	const quizzContent = quizzScreen.querySelector(".container");
+	loadBackgroundImg(quizzContent, selectedQuizz);
+	loadQuizzTitle(quizzContent, selectedQuizz);
+	loadQuizzQuestions(quizzContent, selectedQuizz);
+	
 }
