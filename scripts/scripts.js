ebf06dfe1fe5bd391fc0f9d6cb3a77ea8e4c5855
Gian <giandcarvalho@gmail.com>
2021-08-14 17:04:25 -0300

    Added function to filter user quizzes and community quizzes

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,42 +1,71 @@
-// global variables
 URL_QUIZZES =
   "https://mock-api.bootcamp.respondeai.com.br/api/v3/buzzquizz/quizzes";
-const communityQuizzes = document.querySelector(
-  ".community-quizzes .quizzes-list"
-);
 
 // gets quizzes from api
 function getQuizzes() {
   const promise = axios.get(URL_QUIZZES);
 
-  promise.then(renderQuizzes);
+  promise.then(processQuizzes);
   promise.catch(handleError);
 }
 
-// insert quizzes into the list of community quizzes
-function renderQuizzes(response) {
+// filters which quizz belongs to user and which doesn't
+function processQuizzes(response) {
+  const communityQuizzes = document.querySelector(
+    ".community-quizzes .quizzes-list"
+  );
   const quizzes = response.data;
+  const listId = getIdList();
+  const userQuizzList = quizzes.filter((quizz) => listId.includes(quizz));
+  const communityQuizzList = quizzes.filter((quizz) => !listId.includes(quizz));
+
+  if (listId.length > 0) {
+    renderUserQuizzes(userQuizzList);
+  }
 
   communityQuizzes.innerHTML = "";
+  communityQuizzList.forEach((quizz) => {
+    render(quizz, communityQuizzes);
+  });
+}
 
-  for (let i = 0; quizzes.length > i; i++) {
-    // function "selectQuizzes" inside li is not implemented yet (screen 2)
-    communityQuizzes.innerHTML += `
-        <li onclick="selectQuizz(${quizzes[i].id});">
-				  <img src="${quizzes[i].image}" />                                                                                                     
+//renders user quizzes
+function renderUserQuizzes(userQuizzList) {
+  showFilledUserList();
+
+  userQuizzes.innerHTML = "";
+  userQuizzList.forEach((quizz) => {
+    render(quizz, userQuizzes);
+  });
+}
+
+//changes hides empty list and displays 'filled' user list design
+function showFilledUserList() {
+  const userQuizzes = document.querySelector(".user-quizzes .quizzes-list");
+  const userQuizzesEmpty = document.querySelector(".user-quizzes .empty");
+
+  userQuizzes.parentNode.classList.remove("hidden");
+  userQuizzesEmpty.classList.add("hidden");
+}
+
+//dynamically renders quizz list
+function render(quizz, quizzList) {
+  quizzList.innerHTML += `
+        <li onclick="selectQuizz(${quizz.id});">
+				  <img src="${quizz.image}" />                                                                                                     
 				  <div class="gradient">
 					<p>
-					  ${quizzes[i].title}
+					  ${quizz.title}
 					</p>
 				  </div>
 				</li>`;
-  }
 }
 
 function handleError(error) {
   alert(error.response.data);
 }
 
+//changes homescreen to quizz screen when you click on a quizz - UNCOMMENT the lines below to work
 function selectQuizz(quizzID) {
   const promise = axios.get(`${URL_QUIZZES}/${quizzID}`);
 
@@ -45,6 +74,7 @@ function selectQuizz(quizzID) {
   // promise.catch(handleError);
 }
 
+//shows loading screen
 function showLoadingScreen() {
   const homeScreen = document.querySelector(".screen-1");
   const loading = document.querySelector(".loading");
@@ -53,3 +83,26 @@ function showLoadingScreen() {
   loading.classList.remove("hidden");
 }
 getQuizzes();
+
+// gets id from local storage
+function getIdList() {
+  let idList = localStorage.getItem("idList");
+
+  if (idList !== null) {
+    idList = JSON.parse(idList);
+    return idList;
+  } else {
+    return [];
+  }
+}
+
+//the following function stores ids in the localstorage
+// function storeId(promise) {
+//   let id = promise.data.id;
+//   let idList = getIdList();
+
+//   idList.push(id);
+
+//   localStorage.setItem("idList", JSON.stringify(idList));
+//   getQuizzes();
+// }
--- scripts/selected-quizz.js
+++ scripts/selected-quizz.js
@@ -1,138 +1,139 @@
 const quizzScreen = document.querySelector(".screen-2");
 
 function showQuizzScreen() {
-	const loading = document.querySelector(".loading");
-	const quizzesScreen = document.querySelector(".screen-1");
-	quizzesScreen.classList.add("hidden");
-	//loading.classList.add("hidden");
-	// const quizzScreen = document.querySelector(".screen-2");
-	quizzScreen.classList.remove("hidden");
+  const loading = document.querySelector(".loading");
+  const quizzesScreen = document.querySelector(".screen-1");
+  quizzesScreen.classList.add("hidden");
+  loading.classList.add("hidden");
+  // const quizzScreen = document.querySelector(".screen-2");
+  quizzScreen.classList.remove("hidden");
 }
 
 // QUIZZ FOR TEST:
 const friends = axios.get(
-	"https://mock-api.bootcamp.respondeai.com.br/api/v3/buzzquizz/quizzes/1"
+  "https://mock-api.bootcamp.respondeai.com.br/api/v3/buzzquizz/quizzes/1"
 );
 friends.then(load);
 
 function loadBackgroundImg(quizzContent, selectedQuizz) {
-	const bgImg = selectedQuizz.data.image;
-	const quizzHeader = quizzContent.querySelector(".selected-quizz");
-	quizzHeader.style.backgroundImage = `url('${bgImg}')`;
+  const bgImg = selectedQuizz.data.image;
+  const quizzHeader = quizzContent.querySelector(".selected-quizz");
+  quizzHeader.style.backgroundImage = `url('${bgImg}')`;
 }
 
 function loadQuizzTitle(quizzContent, selectedQuizz) {
-	const quizzTitle = selectedQuizz.data.title;
-	const title = quizzContent.querySelector("h1");
-	title.innerHTML = `${quizzTitle}`;
+  const quizzTitle = selectedQuizz.data.title;
+  const title = quizzContent.querySelector("h1");
+  title.innerHTML = `${quizzTitle}`;
 }
 
 const correctAnswers = [];
 
 function loadQuizzQuestions(quizzContent, selectedQuizz) {
-	const quizzQuestions = selectedQuizz.data.questions;
-	const quizz = quizzContent.querySelector(".quizz");
+  const quizzQuestions = selectedQuizz.data.questions;
+  const quizz = quizzContent.querySelector(".quizz");
 
-	for (let i = 0; i < quizzQuestions.length; i++) {
-		// Load each question
-		quizz.innerHTML +=
-			`<div class="question" id="${i}"> 
+  for (let i = 0; i < quizzQuestions.length; i++) {
+    // Load each question
+    quizz.innerHTML += `<div class="question" id="${i}"> 
 			<h1 style="background-color: ${quizzQuestions[i].color}">${quizzQuestions[i].title}</h1>
 			<div class="options">
 			</div>
-		</div>`
+		</div>`;
 
-		let options = quizz.lastChild.querySelector(".options");
-		let answers = quizzQuestions[i].answers;
+    let options = quizz.lastChild.querySelector(".options");
+    let answers = quizzQuestions[i].answers;
 
-		//Randomize answers
-		answers.sort(function () {
-			return Math.random() - 0.5;
-		});
+    //Randomize answers
+    answers.sort(function () {
+      return Math.random() - 0.5;
+    });
 
-		for (let j = 0; j < answers.length; j++) {
-			//Load each answer			
-			options.innerHTML +=
-			`<button class="option" id="${j}" onclick="selectAnswer(this)">
+    for (let j = 0; j < answers.length; j++) {
+      //Load each answer
+      options.innerHTML += `<button class="option" id="${j}" onclick="selectAnswer(this)">
 				<img src="${answers[j].image}">
 				<p>${answers[j].text}</p>
 			</button>`;
 
-			//Save correct answer in a global variable
-			if(answers[j].isCorrectAnswer){
-				let correctAnswer = {
-					question: i,
-					correctOption: j
-				};
-				correctAnswers.push(correctAnswer);
-			}
-		}
-	}
+      //Save correct answer in a global variable
+      if (answers[j].isCorrectAnswer) {
+        let correctAnswer = {
+          question: i,
+          correctOption: j,
+        };
+        correctAnswers.push(correctAnswer);
+      }
+    }
+  }
 }
 
-function scrollToNextQuestion(id){
-	const questions = quizzScreen.querySelectorAll('.question');
-	const nextId = Number(id)+1;
-	const nextQuestion = questions[nextId];
-	nextQuestion.scrollIntoView({behavior: "smooth", block: "center", inline: "start"});
+function scrollToNextQuestion(id) {
+  const questions = quizzScreen.querySelectorAll(".question");
+  const nextId = Number(id) + 1;
+  const nextQuestion = questions[nextId];
+  nextQuestion.scrollIntoView({
+    behavior: "smooth",
+    block: "center",
+    inline: "start",
+  });
 }
 let answerdQuestions = 0;
 
-function markQuestionAsAnswerd(selectedOption){
-	const options = selectedOption.parentNode.querySelectorAll(".option");
-	for (let i = 0; i < options.length; i++) {
-		//Add blur to the unselected options
-		if( options[i] !== selectedOption){
-			options[i].classList.add("blur")
-		}
-		//Removed on click on all options
-		options[i].removeAttribute("onclick");
+function markQuestionAsAnswerd(selectedOption) {
+  const options = selectedOption.parentNode.querySelectorAll(".option");
+  for (let i = 0; i < options.length; i++) {
+    //Add blur to the unselected options
+    if (options[i] !== selectedOption) {
+      options[i].classList.add("blur");
+    }
+    //Removed on click on all options
+    options[i].removeAttribute("onclick");
 
-		//Change the colors of the answer texts
-		const question = selectedOption.parentNode.parentNode;
-		const correctAnswer = correctAnswers[question.id].correctOption;
+    //Change the colors of the answer texts
+    const question = selectedOption.parentNode.parentNode;
+    const correctAnswer = correctAnswers[question.id].correctOption;
 
-		if(Number(options[i].id) === correctAnswer){
-			let text = options[i].querySelector("p");
-			text.style.color = 'green';
-		}
-		else {
-			let text = options[i].querySelector("p");
-			text.style.color = 'red';
-		}
+    if (Number(options[i].id) === correctAnswer) {
+      let text = options[i].querySelector("p");
+      text.style.color = "green";
+    } else {
+      let text = options[i].querySelector("p");
+      text.style.color = "red";
+    }
 
-		//Scroll to next question after 2 seconds
-		setTimeout(scrollToNextQuestion, 2000, question.id);
-	}
+    //Scroll to next question after 2 seconds
+    setTimeout(scrollToNextQuestion, 2000, question.id);
+  }
 }
 
 let hits = 0;
 
-function selectAnswer(selectedOption){
-	markQuestionAsAnswerd(selectedOption);
-	
-	const isCorrect = selectedOption.querySelector("p").getAttribute("style") === "color: green;"
+function selectAnswer(selectedOption) {
+  markQuestionAsAnswerd(selectedOption);
 
-	const totalQuestions = quizzScreen.querySelectorAll(".question").length;
+  const isCorrect =
+    selectedOption.querySelector("p").getAttribute("style") === "color: green;";
 
-	if(isCorrect){
-		hits++;
-	}
-	
-	//Count as answerd
-	answerdQuestions++;
+  const totalQuestions = quizzScreen.querySelectorAll(".question").length;
 
-	if(answerdQuestions === totalQuestions){
-		console.log("cabou")
-	}
+  if (isCorrect) {
+    hits++;
+  }
 
+  //Count as answerd
+  answerdQuestions++;
+
+  if (answerdQuestions === totalQuestions) {
+    console.log("cabou");
+  }
 }
 
 function load(selectedQuizz) {
-	showQuizzScreen();
+  showQuizzScreen();
 
-	const quizzContent = quizzScreen.querySelector(".container");
-	loadBackgroundImg(quizzContent, selectedQuizz);
-	loadQuizzTitle(quizzContent, selectedQuizz);
-	loadQuizzQuestions(quizzContent, selectedQuizz);
+  const quizzContent = quizzScreen.querySelector(".container");
+  loadBackgroundImg(quizzContent, selectedQuizz);
+  loadQuizzTitle(quizzContent, selectedQuizz);
+  loadQuizzQuestions(quizzContent, selectedQuizz);
 }
