e76c3e47939d44e8f4df2ea939a0bce0cc5cb5d5
Yasmim Cavalcanti <yasmimcavalcanti.r@gmail.com>
2021-08-14 16:58:56 -0300

    Refactored selected-quizz.js

--- scripts/selected-quizz.js
+++ scripts/selected-quizz.js
@@ -1,123 +1,50 @@
-const quizzScreen = document.querySelector(".screen-2");
-
-function showQuizzScreen() {
-	const loading = document.querySelector(".loading");
-	const quizzesScreen = document.querySelector(".screen-1");
-	quizzesScreen.classList.add("hidden");
-	//loading.classList.add("hidden");
-	// const quizzScreen = document.querySelector(".screen-2");
-	quizzScreen.classList.remove("hidden");
-}
-
 // QUIZZ FOR TEST:
 const friends = axios.get(
 	"https://mock-api.bootcamp.respondeai.com.br/api/v3/buzzquizz/quizzes/1"
 );
 friends.then(load);
+// 
+// 
+// 
 
-function loadBackgroundImg(quizzContent, selectedQuizz) {
-	const bgImg = selectedQuizz.data.image;
-	const quizzHeader = quizzContent.querySelector(".selected-quizz");
-	quizzHeader.style.backgroundImage = `url('${bgImg}')`;
-}
+const quizzScreen = document.querySelector(".screen-2");
 
-function loadQuizzTitle(quizzContent, selectedQuizz) {
-	const quizzTitle = selectedQuizz.data.title;
-	const title = quizzContent.querySelector("h1");
-	title.innerHTML = `${quizzTitle}`;
+function scrollToNextQuestion(thisQuestion){	
+	const lastQuestion = quizzScreen.querySelector(".quizz").lastChild;
+	if(thisQuestion === lastQuestion){
+		return;
+	}	
+	const questions = quizzScreen.querySelectorAll('.question');
+	const nextId = Number(thisQuestion.id)+1;
+	const nextQuestion = questions[nextId];
+	nextQuestion.scrollIntoView({behavior: "smooth", block: "center", inline: "start"});
 }
 
 const correctAnswers = [];
 
-function loadQuizzQuestions(quizzContent, selectedQuizz) {
-	const quizzQuestions = selectedQuizz.data.questions;
-	const quizz = quizzContent.querySelector(".quizz");
-
-	for (let i = 0; i < quizzQuestions.length; i++) {
-		// Load each question
-		quizz.innerHTML +=
-			`<div class="question" id="${i}"> 
-			<h1 style="background-color: ${quizzQuestions[i].color}">${quizzQuestions[i].title}</h1>
-			<div class="options">
-			</div>
-		</div>`
-
-		let options = quizz.lastChild.querySelector(".options");
-		let answers = quizzQuestions[i].answers;
-
-		//Randomize answers
-		answers.sort(function () {
-			return Math.random() - 0.5;
-		});
-
-		for (let j = 0; j < answers.length; j++) {
-			//Load each answer			
-			options.innerHTML +=
-			`<button class="option" id="${j}" onclick="selectAnswer(this)">
-				<img src="${answers[j].image}">
-				<p>${answers[j].text}</p>
-			</button>`;
-
-			//Save correct answer in a global variable
-			if(answers[j].isCorrectAnswer){
-				let correctAnswer = {
-					question: i,
-					correctOption: j
-				};
-				correctAnswers.push(correctAnswer);
-			}
-		}
-	}
-}
-
-function scrollToNextQuestion(id){
-	const questions = quizzScreen.querySelectorAll('.question');
-	const nextId = Number(id)+1;
-	const nextQuestion = questions[nextId];
-	nextQuestion.scrollIntoView({behavior: "smooth", block: "center", inline: "start"});
-}
-let answerdQuestions = 0;
-
 function markQuestionAsAnswerd(selectedOption){
+	const thisQuestion = selectedOption.parentNode.parentNode;
 	const options = selectedOption.parentNode.querySelectorAll(".option");
-	const question = selectedOption.parentNode.parentNode;
-	for (let i = 0; i < options.length; i++) {
+	const correctAnswer = correctAnswers[thisQuestion.id].correctOption;
+
+	options.forEach(option => {
 		//Add blur to the unselected options
-		if( options[i] !== selectedOption){
-			options[i].classList.add("blur")
+		if(option !== selectedOption){
+			option.classList.add("blur")
 		}
 		//Removed on click on all options
-		options[i].removeAttribute("onclick");
+		option.removeAttribute("onclick");
 
 		//Change the colors of the answer texts		
-		const correctAnswer = correctAnswers[question.id].correctOption;
-
-		if(Number(options[i].id) === correctAnswer){
-			let text = options[i].querySelector("p");
+		let text = option.querySelector("p");
+		if(Number(option.id) === correctAnswer){			
 			text.style.color = 'green';
 		}
 		else {
-			let text = options[i].querySelector("p");
 			text.style.color = 'red';
 		}		
-	}
-
-	const lastQuestion = quizzScreen.querySelector(".quizz").lastChild;
-	if(question === lastQuestion){
-		return;
-	}
-	//Scroll to next question after 2 seconds
-	setTimeout(scrollToNextQuestion, 2000, question.id);
-}
-
-let hits = 0;
-
-const levels = [];
-
-function saveLevels(selectedQuizz){
-	selectedQuizz.data.levels.forEach(level => {
-		levels.push(level);
 	});
+	setTimeout(scrollToNextQuestion, 2000, thisQuestion);
 }
 
 function restartBtn(){
@@ -128,21 +55,13 @@ function restartBtn(){
 function homeBtn(){
 	const quizzesScreen = document.querySelector(".screen-1");
 	quizzScreen.classList.add("hidden");
-
 	//loading.classList.add("hidden");
 	quizzesScreen.classList.remove("hidden");
 	quizzesScreen.scrollIntoView(true);
 }
 
-function showResult(totalQuestions){
-	const resultPercent =  Math.round(hits/totalQuestions*100);
-
-	const result = levels.reverse().find( level => {
-		return resultPercent>=level.minValue;
-	})
-
+function renderizeResultBox(result, resultPercent){
 	const quizz = quizzScreen.querySelector(".quizz");
-
 	quizz.innerHTML += 
 	`<div class="result"> 
 		<h1 style="background-color: red">${resultPercent}% de acerto: ${result.title}</h1>
@@ -158,29 +77,103 @@ function showResult(totalQuestions){
 	<button class="home-btn" onclick="homeBtn();">
 		Voltar pra home
 	</button>`;
-	
-	const resultBox = quizz.querySelector(".result");
+}
 
+let hits = 0;
+
+function showResult(totalQuestions){
+	const resultPercent =  Math.round(hits/totalQuestions*100);
+	const result = levels.find(level => resultPercent >= level.minValue);
+	renderizeResultBox(result, resultPercent);	
+	const resultBox = quizzScreen.querySelector(".quizz .result");
 	resultBox.scrollIntoView({behavior: "smooth", block: "center", inline: "start"});
 }
 
+let answerdQuestions = 0;
+
 function selectAnswer(selectedOption){
-	markQuestionAsAnswerd(selectedOption);
-	
-	const isCorrect = selectedOption.querySelector("p").getAttribute("style") === "color: green;"
-
-	const totalQuestions = quizzScreen.querySelectorAll(".question").length;
-
+	markQuestionAsAnswerd(selectedOption);	
+	const isCorrect = selectedOption.querySelector("p").getAttribute("style") === "color: green;";
 	if(isCorrect){
 		hits++;
-	}
-	
-	//Count as answerd
+	}	
 	answerdQuestions++;
 
+	const totalQuestions = quizzScreen.querySelectorAll(".question").length;
 	if(answerdQuestions === totalQuestions){
 		setTimeout(showResult, 2000, totalQuestions);
 	}
+}
+
+function showQuizzScreen() {
+	const loading = document.querySelector(".loading");
+	const quizzesScreen = document.querySelector(".screen-1");
+	quizzesScreen.classList.add("hidden");
+	//loading.classList.add("hidden");
+	// const quizzScreen = document.querySelector(".screen-2");
+	quizzScreen.classList.remove("hidden");
+}
+
+function loadBackgroundImg(quizzContent, selectedQuizz) {
+	const bgImg = selectedQuizz.data.image;
+	const quizzHeader = quizzContent.querySelector(".selected-quizz");
+	quizzHeader.style.backgroundImage = `url('${bgImg}')`;
+}
+
+function loadQuizzTitle(quizzContent, selectedQuizz) {
+	const quizzTitle = selectedQuizz.data.title;
+	const title = quizzContent.querySelector("h1");
+	title.innerHTML = `${quizzTitle}`;
+}
+
+function saveIfIsCorrectAnswer(answer, questionId, answerId){
+	if(answer.isCorrectAnswer){
+		let correctAnswer = {
+			question: questionId,
+			correctOption: answerId
+		};
+		correctAnswers.push(correctAnswer);
+	}
+}
+
+function renderizeAnswers(question, questionId, quizz){
+	let options = quizz.lastChild.querySelector(".options");
+	let answers = question.answers;
+	answers.sort(() => Math.random() - 0.5);
+
+	answers.forEach(function(answer, answerId) {		
+		options.innerHTML +=
+		`<button class="option" id="${answerId}" onclick="selectAnswer(this)">
+			<img src="${answer.image}">
+			<p>${answer.text}</p>
+		</button>`;
+
+		saveIfIsCorrectAnswer(answer, questionId, answerId);
+	});		
+}
+
+function loadQuizzQuestions(quizzContent, selectedQuizz) {
+	const quizzQuestions = selectedQuizz.data.questions;
+	const quizz = quizzContent.querySelector(".quizz");
+
+	quizzQuestions.forEach(function(question, questionId) {
+		quizz.innerHTML +=
+			`<div class="question" id="${questionId}"> 
+			<h1 style="background-color: ${question.color}">${question.title}</h1>
+			<div class="options">
+			</div>
+		</div>`
+		renderizeAnswers(question, questionId, quizz);
+	});
+}
+
+const levels = [];
+
+function saveLevels(selectedQuizz){
+	selectedQuizz.data.levels.forEach(level => {
+		levels.push(level);
+	});
+	levels.reverse()
 }
 
 function load(selectedQuizz) {
@@ -190,6 +183,5 @@ function load(selectedQuizz) {
 	loadBackgroundImg(quizzContent, selectedQuizz);
 	loadQuizzTitle(quizzContent, selectedQuizz);
 	loadQuizzQuestions(quizzContent, selectedQuizz);
-
 	saveLevels(selectedQuizz);
 }
